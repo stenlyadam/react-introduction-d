ced17764fdaa53af8492028432e15c048ff3f780
Stenly Adam <stenly.adam@unklab.ac.id>
2025-11-04 16:45:08 +0800

    click event & state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 //Komponen adalah fungsi yang return JSX
+import { useState } from "react";
 import HelloWorld from "./HelloWorld";
 
 const App = () => {
-  let hello = "Hello World";
+  // let hello = "Hello World"; //ini contoh deklarasi variabel biasa
+  const [hello, setHello] = useState("Hello World !!!"); //ini contoh deklarasi variabel state
+
+  const handleClick = () => {
+    console.log("Button di Klik");
+    // hello = "Selamat Datang"; //ini cara merubah variabel biasa
+    setHello("Selamat Datang"); // ini cara merubah variabel state
+    // console.log(hello);
+  };
   return (
     <>
       <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
         {hello}
       </div>
-      <HelloWorld text="Ini adalah komponen Hello World 1" />
-      <HelloWorld text="Ini adalah komponen Hello World 2" />
+      <HelloWorld
+        text="Ini adalah komponen Hello World 1"
+        handleClick={handleClick}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded-xl text-white"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
     </>
   );
 };
